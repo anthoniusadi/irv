7ae66e4a8a548ad0326e92db1f63d9057349be0c
anthoniusadi <anthonyfix0@gmail.com>
2022-10-20 11:34:10 +0700

    fix main_program.py process each file and plot

--- main_program.py
+++ main_program.py
@@ -15,15 +15,21 @@ import os
 # plot(y,save=True,name_save='vibration_wave')
 
 # process each file in folder
-folderName_audio = 'data/lebokekenedatane/raw_au/'
+folderName_audio = 'data/lebokekenedatane/raw_au'
 for f in os.listdir(folderName_audio):
-    audio_preprocessing = preprocessing(f,'output_audio',8000)
+    p_file = os.path.join(folderName_audio,f)
+    print(p_file)
+    audio_preprocessing = preprocessing(p_file,'output_audio',8000)
     y = audio_preprocessing.audio()
+    plot(y,save=True,name_save=f)
+
     print(f'{f} is process')
 
 folderName_acel = 'data/lebokekenedatane/raw_acc/'
-for f in os.listdir(folderName_audio):
-    vib_preprocessing = preprocessing(f,'output_vib',400)
+for f in os.listdir(folderName_acel):
+    p_file = os.path.join(folderName_acel,f)
+    print(p_file)
+    vib_preprocessing = preprocessing(p_file,'output_vib',400)
     y=vib_preprocessing.vibration()
-    plot(y,save=True,name_save='vibration_wave')
+    plot(y,save=True,name_save=f)
     print(f'{f} is process')
